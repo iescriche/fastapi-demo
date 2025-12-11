17cafcf9f906ec599493429f49f243f93ba2d523
iescriche <iescriche@laberit.com>
2025-12-11 13:10:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hola desde FastAPI!"}
+class House(BaseModel):
+    size: float
+    rooms: int
+    bathrooms: int
+
+@app.post("/predict")
+def predict(data: House):
+    # EJEMPLO → fórmula simple
+    prediction = data.size * 1200 + data.rooms * 5000 + data.bathrooms * 8000
+    return {"prediction": prediction}
